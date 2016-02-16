8e133e1852d5055a9d5090fffa7971a4e615933a
kociumbas <kociumbas@outlook.com>
2016-02-16 01:05:08 +0100

    CSPiSqrtConsole

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_3PierwiastekZPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = Math.PI;
+            double root = Math.Sqrt(pi);
+            Console.WriteLine("Pierwiastek kwadratowy z liczby Pi = {0:#.##}", root);
+            Console.ReadLine();
+        }
+    }
+}
